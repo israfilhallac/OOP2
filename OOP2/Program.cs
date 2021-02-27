30eb6a76decad87e885d936c50ab53a02010227d
İsrafilHallac <i.hallac1408@gmail.com>
2021-02-27 09:52:43 +0200

    Add project files.

---
+++ OOP2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            İndividualCustomer individualCustomer1 = new İndividualCustomer();
+            individualCustomer1.Id = 1;
+            individualCustomer1.CustomerNo = "4";
+            individualCustomer1.CitizenshipNumber = "1234564";
+            individualCustomer1.Name = "İsrafil";
+            individualCustomer1.SurName = "Hallaç";
+
+            CoorporateCustomer coorporateCustomer1 = new CoorporateCustomer();
+            coorporateCustomer1.Id = 2;
+            coorporateCustomer1.CustomerNo = "12354";
+            coorporateCustomer1.CompanyName = "BTK";
+            coorporateCustomer1.TaxNumber = "25687";
+
+            Console.WriteLine("Müsteri Adı : " +individualCustomer1.Name);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ OOP2/İndividualCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class İndividualCustomer:Customer
+
+    {
+        public string CitizenshipNumber { get; set; }
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+    }
+}
